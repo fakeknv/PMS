06cdf4d5b13b44a0c0d2a3b24a4ff7db35758f2c
Kim Vicera <Kim Vicera@Knvenient>
2018-09-04 15:10:16 -0700

    Initial commit. Contains the bare structure and layout of the project.

---
+++ PRMS/UIManager/Login.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace PRMS.UIManager
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
