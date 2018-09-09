51bf5dab2af36dcff170c1e4a10c6dba338fb13e
gourgaem <knvenient@gmail.com>
2018-09-09 20:58:29 +0800

    Added Notification Item on Notification View.

--- PMS/UIManager/Views/DirectoryOfTheDeceased.cs
+++ PMS/UIManager/Views/DirectoryOfTheDeceased.cs
@@ -5,9 +5,9 @@ namespace PMS.UIManager.Views
 	/// <summary>
 	/// Interaction logic for Schedules.xaml
 	/// </summary>
-	public partial class Schedules : UserControl
+	public partial class DirectoryOfTheDeceased : UserControl
 	{
-		public Schedules()
+		public DirectoryOfTheDeceased()
 		{
 			InitializeComponent();
 		}
--- PMS/UIManager/Views/Notifications.xaml.cs
+++ PMS/UIManager/Views/Notifications.xaml.cs
@@ -14,8 +14,18 @@ namespace PMS.UIManager.Views
 			InitializeComponent();
 			NotificationItem x1 = new NotificationItem();
 			x1.Name = "TestingNotif";
-			x1.NotifContent = "TESTd";
-			//Main.Items.Add(x1);
-		}
-	}
+			x1.NotifContent = "Test Notification 1";
+
+            var listBox = new ListBox();
+            NotifContainer.Content = listBox;
+            listBox.Items.Add(x1);
+            NotificationItem x2 = new NotificationItem();
+            x2.Name = "TestingNotif2";
+            x2.NotifContent = "Test Notification 2";
+            listBox.Items.Add(x2);
+            //Main.Items.Add(x1);
+            FilterButton.Items.Add("asd");
+        }
+
+    }
 }
