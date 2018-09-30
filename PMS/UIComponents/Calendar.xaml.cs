c23c55b656ee6d0fb9358e77dcdbd8da586e05c3
gourgaem <knvenient@gmail.com>
2018-09-30 20:08:28 +0800

    Fixed Calendar offset. Added next and previous month button. Redesigned UI.

--- PMS/UIComponents/Calendar.xaml.cs
+++ PMS/UIComponents/Calendar.xaml.cs
@@ -13,9 +13,13 @@ namespace PMS.UIComponents
     {
 		public ObservableCollection<Day> Days { get; set; }
 		public ObservableCollection<string> DayNames { get; set; }
+
+		public DateTime ActiveDate = DateTime.Today;
 		public Calendar()
         {
             InitializeComponent();
+			monthNameHolder.Content = DateTime.Today.ToString("MMMM yyyy");
+
 			DayNames = new ObservableCollection<string> { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
 
 			test1x.ItemsSource = DayNames;
@@ -29,8 +33,11 @@ namespace PMS.UIComponents
 
 			DateTime d = new DateTime(targetDate.Year, targetDate.Month, 1);
 			int offset = DayOfWeekNumber(d.DayOfWeek);
-			if (offset != 1) d = d.AddDays(-offset);
-
+			//if (offset != 1) broke October 2018
+			if (offset != 0)
+			{
+				d = d.AddDays(-offset);
+			}
 			//Show 6 weeks each with 7 days = 42
 			for (int box = 1; box <= 42; box++)
 			{
@@ -44,6 +51,19 @@ namespace PMS.UIComponents
 		private static int DayOfWeekNumber(DayOfWeek dow)
 		{
 			return Convert.ToInt32(dow.ToString("D"));
+		}
+
+		private void NextMonthButton_Click(object sender, RoutedEventArgs e)
+		{
+			ActiveDate = ActiveDate.AddMonths(1);
+			monthNameHolder.Content = ActiveDate.ToString("MMMM yyyy");
+			BuildCalendar(ActiveDate);
+		}
+		private void PrevMonthButton_Click(object sender, RoutedEventArgs e)
+		{
+			ActiveDate = ActiveDate.AddMonths(-1);
+			monthNameHolder.Content = ActiveDate.ToString("MMMM yyyy");
+			BuildCalendar(ActiveDate);
 		}
 	}
 }
