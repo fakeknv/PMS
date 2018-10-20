f9ae963f4a096d22a9925b9a47230f86cee57692
gourgaem <knvenient@gmail.com>
2018-10-21 04:56:17 +0800

    Finished retrieving Registers data from db.

--- PMS/UIManager/Views/Registers.xaml.cs
+++ PMS/UIManager/Views/Registers.xaml.cs
@@ -1,4 +1,7 @@
-﻿using PMS.UIComponents;
+﻿using MySql.Data.MySqlClient;
+using PMS.UIComponents;
+using System;
+using System.Data;
 using System.Windows.Controls;
 using System.Windows.Media;
 
@@ -9,15 +12,78 @@ namespace PMS.UIManager.Views
 	/// </summary>
 	public partial class Registers : UserControl
 	{
+		//MYSQL
+		DBConnectionManager dbman;
+
 		public Registers()
 		{
 			InitializeComponent();
-			for (int i=0; i<10; i++) { 
-			RegisterItem ri = new RegisterItem();
-			test1.Items.Add(ri);
+			SyncRegisters();
+		}
+		private string CountEntries(int bookNum)
+		{
+			string ret = "0";
+			dbman = new DBConnectionManager();
+			if (dbman.DBConnect().State == ConnectionState.Open)
+			{
+				MySqlCommand cmd = dbman.DBConnect().CreateCommand();
+				cmd.CommandText = "SELECT COUNT(*) FROM records WHERE book_number = @book_number;";
+				cmd.Parameters.AddWithValue("@book_number", bookNum);
+				cmd.Prepare();
+				MySqlDataReader db_reader = cmd.ExecuteReader();
+				while (db_reader.Read())
+				{
+					ret = db_reader.GetString("COUNT(*)");
+				}
+				//close Connection
+				dbman.DBClose();
+			}
+			return ret;
+		}
+		private string CountPages(int bookNum)
+		{
+			string ret = "0";
+			dbman = new DBConnectionManager();
+			if (dbman.DBConnect().State == ConnectionState.Open)
+			{
+				MySqlCommand cmd = dbman.DBConnect().CreateCommand();
+				cmd.CommandText = "SELECT * FROM records WHERE book_number = @book_number ORDER BY page_number DESC LIMIT 1;";
+				cmd.Parameters.AddWithValue("@book_number", bookNum);
+				cmd.Prepare();
+				MySqlDataReader db_reader = cmd.ExecuteReader();
+				while (db_reader.Read())
+				{
+					ret = db_reader.GetString("page_number");
+				}
+				//close Connection
+				dbman.DBClose();
+			}
+			return ret;
+		}
+		private void SyncRegisters()
+		{
+			dbman = new DBConnectionManager();
+			if (dbman.DBConnect().State == ConnectionState.Open)
+			{
+				MySqlCommand cmd = dbman.DBConnect().CreateCommand();
+				cmd.CommandText = "SELECT * FROM registers;";
+				MySqlDataReader db_reader = cmd.ExecuteReader();
+				while (db_reader.Read())
+				{
+					RegisterItem ri = new RegisterItem();
+					ri.BookTypeHolder.Content = db_reader.GetString("book_type");
+					ri.BookNoHolder.Content = "Book #" + db_reader.GetString("book_number");
+					ri.BookContentStatHolder.Content = CountEntries(Convert.ToInt32(db_reader.GetString("book_number"))) + " Entries | " + CountPages(Convert.ToInt32(db_reader.GetString("book_number"))) + " Pages";
+					RegistersContainer.Items.Add(ri);
+				}
+				//close Connection
+				dbman.DBClose();
+			}
+			else
+			{
+
 			}
 		}
-
 		private void CreateRequestButton_Click(object sender, System.Windows.RoutedEventArgs e)
 		{
 
