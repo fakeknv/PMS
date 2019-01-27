0952cf2d9232a7e693048afb6ef6900dacb57f08
gourgaem <knvenient@gmail.com>
2019-01-27 19:03:57 +0800

    Added login for custom privileged account.

--- PMS/UIManager/UIManager.xaml.cs
+++ PMS/UIManager/UIManager.xaml.cs
@@ -33,7 +33,7 @@ namespace PMS.UIManager
 		}
 		private void InitPriv() {
 			HamburgerMenuItemCollection itemCollection = HamburgerMenuControl.ItemsSource as HamburgerMenuItemCollection;
-			
+
 			if (Convert.ToInt32(Application.Current.Resources["priv"]) == 1) {
 				//Admin
 				itemCollection.Remove(Transactions);
@@ -84,6 +84,37 @@ namespace PMS.UIManager
 				itemCollection.Remove(Timeslots);
 				itemCollection.Remove(AppointmentTypes);
 				itemCollection.Remove(ControlPanel);
+			}
+			else if (Convert.ToInt32(Application.Current.Resources["priv"]).ToString().Contains("6"))
+			{
+				itemCollection.Remove(Appointments);
+				itemCollection.Remove(Search);
+				itemCollection.Remove(Transactions);
+				itemCollection.Remove(Registers);
+				itemCollection.Remove(Directory);
+				itemCollection.Remove(Archives);
+				itemCollection.Remove(Reports);
+				itemCollection.Remove(Accounts);
+				itemCollection.Remove(Priests);
+				itemCollection.Remove(Timeslots);
+				itemCollection.Remove(AppointmentTypes);
+				itemCollection.Remove(ControlPanel);
+				if (Convert.ToInt32(Application.Current.Resources["priv"]).ToString().Contains("2")) {
+					//Secretary
+					itemCollection.Add(Appointments);
+				}
+				if (Convert.ToInt32(Application.Current.Resources["priv"]).ToString().Contains("3"))
+				{
+					//Cashier
+					itemCollection.Add(Transactions);
+				}
+				if (Convert.ToInt32(Application.Current.Resources["priv"]).ToString().Contains("4"))
+				{
+					//Registrar
+					itemCollection.Add(Search);
+					itemCollection.Add(Registers);
+					itemCollection.Add(Directory);
+				}
 			}
 		}
         private void HamburgerMenuControl_OnItemClick(object sender, ItemClickEventArgs e)
